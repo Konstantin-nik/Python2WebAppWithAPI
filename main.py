2897880b4a3087c30f8faff04574ca59f3d4bd2f
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2023-12-11 00:41:57 +0300

    add translated page and tet tts option

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
 from flask import Flask, render_template, request
+from google.cloud import texttospeech
+from google.cloud import translate_v2 as translate
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_file('bcnpy2key.json')
 
 app = Flask(__name__)
+
+client = texttospeech.TextToSpeechClient(credentials=credentials)
+
+
+def translate_text(target: str, text: str) -> dict:
+
+    translate_client = translate.Client()
+
+    if isinstance(text, bytes):
+        text = text.decode("utf-8")
+
+    result = translate_client.translate(text, target_language=target)
+
+    print("Text: {}".format(result["input"]))
+    print("Translation: {}".format(result["translatedText"]))
+    print("Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+    return result
+
+
+def tts(text, language):
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code=language, ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    with open("output.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
 
 
 @app.route('/')
@@ -9,9 +51,12 @@ def index():
 
 
 @app.route('/translate', methods=['POST'])
-def translate():
+def translate_m():
     text_to_translate = request.form['text_to_translate']
     target_language = request.form['target_language']
+
+    return render_template('translated.html', original_text=text_to_translate,
+                           translated_text=target_language)
 
 
 if __name__ == '__main__':
