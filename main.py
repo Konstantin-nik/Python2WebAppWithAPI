49c8a628f5853724bc32353065c78922f426557d
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2023-12-11 01:04:57 +0300

    add translation

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from google.cloud import texttospeech
 from google.cloud import translate_v2 as translate
 from google.oauth2 import service_account
+from pydub import AudioSegment
+from pydub.playback import play
 
 credentials = service_account.Credentials.from_service_account_file('bcnpy2key.json')
 
@@ -12,7 +14,7 @@ client = texttospeech.TextToSpeechClient(credentials=credentials)
 
 def translate_text(target: str, text: str) -> dict:
 
-    translate_client = translate.Client()
+    translate_client = translate.Client(credentials=credentials)
 
     if isinstance(text, bytes):
         text = text.decode("utf-8")
@@ -29,7 +31,7 @@ def translate_text(target: str, text: str) -> dict:
 def tts(text, language):
     synthesis_input = texttospeech.SynthesisInput(text=text)
     voice = texttospeech.VoiceSelectionParams(
-        language_code=language, ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+        language_code=language
     )
 
     audio_config = texttospeech.AudioConfig(
@@ -40,9 +42,12 @@ def tts(text, language):
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
 
-    with open("output.mp3", "wb") as out:
+    with open("static/output.mp3", "wb") as out:
         out.write(response.audio_content)
         print('Audio content written to file "output.mp3"')
+
+    audio = AudioSegment.from_file("static/output.mp3", format="mp3")
+    play(audio)
 
 
 @app.route('/')
@@ -54,10 +59,12 @@ def index():
 def translate_m():
     text_to_translate = request.form['text_to_translate']
     target_language = request.form['target_language']
-    tts(text_to_translate, target_language)
+
+    result = translate_text(target_language, text_to_translate)
+    tts(result["translatedText"], target_language)
 
     return render_template('translated.html', original_text=text_to_translate,
-                           translated_text=target_language)
+                           translated_text=result["translatedText"], audio_url='output.mp3')
 
 
 if __name__ == '__main__':
